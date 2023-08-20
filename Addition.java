a114db9337c6ad5073aef16ad6e4f74cf56c8bab
Kunal Vinod Pal <kunalpal460@gmail.com>
2023-08-20 14:53:29 +0530

    added addtion.java

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String args[]){
+        int a=5;
+        int b=5;
+        int add = a+b;
+        System.out.println("addition of two number" + add);
+    }
+}
