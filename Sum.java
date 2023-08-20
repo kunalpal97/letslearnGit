6cfd72e984657a1199de1862da90c49ecb7988bc
Kunal Vinod Pal <kunalpal460@gmail.com>
2023-08-20 14:34:17 +0530

    addede something in Sum.java

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ public class Sum {
         int a=5;
         int b=15;
         int add = a+b;
-        System.out.println(add);
+        System.out.println("addition of two number" + add);
     }
 }
